a022a1db966850e05a9d3b1cc9bb7ad644808890
Mark McDonald <markwilliammcdonald@gmail.com>
2020-07-19 17:03:44 -0500

    added the intro questions using Inquirer

--- index.js
+++ index.js
@@ -19,3 +19,32 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
 });
+
+// Function to validate that the questions asked are answered
+function validation(value) {
+    if (value != "") {
+        return true;
+    } else {
+        return "Please answer the question.";
+    }
+}
+
+//Inquirer questions 
+const introQuestion = [
+    {
+        type: "list",
+        name: "intro",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all departments",
+            "View all roles",
+            "Add an employee",
+            "Add a department",
+            "Add an employee role",
+            "Update employee role",
+            "Exit application",
+        ],
+        validate: validation,
+    },
+];
